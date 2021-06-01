bb0eebefd48d662a8c39890f017dfc45861dcfd3
Héctor Sabillón <h.hsabillon@gmail.com>
2021-05-31 18:52:46 -0600

    Creación de la pantalla Home e inicio de la configuración de la API

--- App.js
+++ App.js
@@ -1,21 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import Home from "./src/components/screens/Home";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <>
+      <Home />
       <StatusBar style="auto" />
-    </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/api/index.js
@@ -0,0 +1,9 @@
+// La ruta principal para consultar a la API
+const pokeapi = "https://pokeapi.co/api/v2/";
+
+const fetchPokemons = () => {
+  // https://pokeapi.co/api/v2/pokemon/?limit=150
+  const endpoint = 
+  
+  return 
+}
