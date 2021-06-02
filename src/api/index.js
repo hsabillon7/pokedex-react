52cd61c899f225d42725af56f5b69daf62e2cc49
Héctor Sabillón <h.hsabillon@gmail.com>
2021-06-01 18:54:50 -0600

    Implementación de promises y async/await para la obtención de los pokemon. Implementación de useEffect en el llamado a la API

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,17 @@
 // La ruta principal para consultar a la API
 const pokeapi = "https://pokeapi.co/api/v2/";
 
-const fetchPokemons = () => {
-  // https://pokeapi.co/api/v2/pokemon/?limit=150
-  const endpoint = 
-  
-  return 
-}
+const fetchPokemons = async () => {
+  // Obtendría los 150 pokemon iniciales
+  const endpoint = `${pokeapi}pokemon/?limit=150`;
+
+  // Realizar la petición a la API. Esta petición debe ser asíncrona.
+  const response = await fetch(endpoint);
+  const data = await response.json();
+
+  console.log(data);
+
+  return data;
+};
+
+export default fetchPokemons;
--- src/components/screens/Home.js
+++ src/components/screens/Home.js
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import fetchPokemons from "../../api";
 
 const Home = () => {
+  useEffect(() => {
+    fetchPokemons();
+  }, []);
+
   return (
     <View>
       <Text>Pokedex con React Native y PokeAPI</Text>
