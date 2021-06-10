725b10f2843f4337bafe8b9d00dd608d0deea64b
Héctor Sabillón <h.hsabillon@gmail.com>
2021-06-09 19:06:47 -0600

    Creación de una función de búsqueda para pokemons. Implementación de try/catch para peticiones asíncronas

--- src/api/index.js
+++ src/api/index.js
@@ -3,15 +3,31 @@ import getEnvVars from "../../environment";
 // La ruta principal para consultar a la API
 const { apiUrl } = getEnvVars();
 
-const fetchPokemons = async () => {
-  // Obtendría los 150 pokemon iniciales
-  const endpoint = `${apiUrl}pokemon/?limit=150`;
+export const fetchPokemons = async () => {
+  try {
+    // Obtendría los 150 pokemon iniciales
+    const endpoint = `${apiUrl}pokemon/?limit=150`;
 
-  // Realizar la petición a la API. Esta petición debe ser asíncrona.
-  const response = await fetch(endpoint);
-  const data = await response.json();
+    // Realizar la petición a la API. Esta petición debe ser asíncrona.
+    const response = await fetch(endpoint);
+    const data = await response.json();
 
-  return data;
+    return data;
+  } catch (error) {
+    console.log(error);
+    return { count: 0 };
+  }
 };
 
-export default fetchPokemons;
+export const searchPokemon = async (pokemon) => {
+  try {
+    const endpoint = `{${apiUrl}}pokemon/${pokemon}`;
+
+    const response = await fetch(endpoint);
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
