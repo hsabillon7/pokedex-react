8e68f1b14bbdf397092408fd750f2a38e03cd5f4
Héctor Sabillón <h.hsabillon@gmail.com>
2021-06-15 18:24:00 -0600

    Implementación de React Navigation + Stack Navigator

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import Home from "./src/components/screens/Home";
+import Info from "./src/components/screens/Info";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <>
-      <Home />
-      <StatusBar style="auto" />
-    </>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Info" component={Info} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
