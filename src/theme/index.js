3661463d2075ca02ebf80c9b242dd9a01108a122
Héctor Sabillón <h.hsabillon@gmail.com>
2021-06-21 18:54:52 -0600

    Tema de colores a utilizar en la aplicación mediante react native paper

---
+++ src/theme/index.js
@@ -0,0 +1,30 @@
+import { DefaultTheme } from "react-native-paper";
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    normal: "#a8a8a8",
+    fighting: "#0c3028",
+    flying: "#9096f0",
+    poison: "#a040a0",
+    ground: "#e0b668",
+    rock: "#b8a038",
+    bug: "#9cb820",
+    ghost: "#705898",
+    steel: "#6d8f9c",
+    fire: "#f08030",
+    water: "#6890f0",
+    grass: "#22c02a",
+    electric: "#c09643",
+    psychic: "#eb2d77",
+    ice: "#98d8d8",
+    dragon: "#7038f8",
+    dark: "#504843",
+    fairy: "#f09ad9",
+    unknown: "#66625c",
+    shadow: "#66625c",
+  },
+};
+
+export default theme;
