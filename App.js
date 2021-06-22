d8c61c8e962723d8a222df9b73ed578308434ac5
Héctor Sabillón <h.hsabillon@gmail.com>
2021-06-21 18:57:04 -0600

    Implementación de React Native Paper

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { Provider as PaperProvider } from "react-native-paper";
+import theme from "./src/theme";
 import Home from "./src/components/screens/Home";
 import Info from "./src/components/screens/Info";
 
@@ -8,11 +10,13 @@ const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Info" component={Info} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <PaperProvider theme={theme}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="Info" component={Info} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
   );
 }
